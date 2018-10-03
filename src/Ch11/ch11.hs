d76146d8b2b4e4281fb4f9eafc2f4ef6d37a8baa
Vance Palacio <vanceism7@gmail.com>
2018-10-02 22:31:55 -0700

    Playing with applicatives

--- src/Ch11/ch11.hs
+++ src/Ch11/ch11.hs
@@ -2,4 +2,10 @@ import Control.Monad
 import Control.Applicative
 
 
+sequenceB1 :: (Applicative f) => [f a] -> f [a]
+sequenceB1 [] = pure []
+sequenceB1 (x:xs) =
+   (:) <$> x <*> sequenceB1 xs
 
+sequenceB2 xs =
+  foldr (\x acc -> (:) <$> x <*> acc) (pure []) xs
