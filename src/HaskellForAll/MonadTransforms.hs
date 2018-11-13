8d7e51549bac79652f4f95fb7e3d2dd6b67d5d79
Vance Palacio <vanceism7@gmail.com>
2018-11-13 10:20:52 -0800

    Implemented state for eval

--- src/HaskellForAll/MonadTransforms.hs
+++ src/HaskellForAll/MonadTransforms.hs
@@ -24,6 +24,9 @@ data Value = IntVal Integer
 
 type Env = Map.Map Name Value -- Maps from names to values
 
+exampleExp :: Exp
+exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+
 --Basic Eval function
 eval0 :: Env -> Exp -> Value
 eval0 env (Lit i) = IntVal i
@@ -97,3 +100,91 @@ eval2 env (App e1 e2) = do
   case val1 of
     FunVal env' n body -> eval2 (Map.insert n val2 env') body
     otherwise -> throwError "Type error: value is not a function"
+
+
+----------------------
+-- Reader Extension
+------------
+
+type Eval3 a = ReaderT Env (ExceptT String Identity) a
+
+runEval3 :: Env -> Eval3 a -> Either String a
+runEval3 env e = runIdentity $ runExceptT $ runReaderT e env
+
+eval3 :: Exp -> Eval3 Value
+eval3 (Lit i) = return $ IntVal i
+eval3 (Var n) = do
+  env <- ask
+  case Map.lookup n env of
+    Just x -> return x
+    Nothing -> throwError $ "Unbound variable " ++ n
+
+eval3 (Plus e1 e2) = do
+  i1 <- eval3 e1
+  i2 <- eval3 e2
+  case (i1,i2) of
+    (IntVal a, IntVal b) -> return $ IntVal(a+b)
+    otherwise -> throwError "Type error"
+
+eval3 (Abs n e) = do
+  env <- ask
+  return $ FunVal env n e
+
+eval3 (App e1 e2) = do
+  val1 <- eval3 e1
+  val2 <- eval3 e2
+  case val1 of
+    FunVal env' n body ->
+      local (const (Map.insert n val2 env')) (eval3 body)
+    otherwise ->
+        throwError "Type error: value is not a function"
+
+-------------------
+-- Adding State
+----------
+
+type Eval4 a = ReaderT Env (ExceptT String (StateT Integer Identity)) a
+
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env state eval = runIdentity (runStateT (runExceptT (runReaderT eval env )) state)
+
+tick :: (Num a, MonadState a m) => m ()
+tick = do
+  st <- get
+  put(st + 1)
+
+eval4 :: Exp -> Eval4 Value
+eval4 (Lit i) = do
+  tick
+  return $ IntVal i
+
+eval4 (Var n) = do
+  tick
+  env <- ask
+  case Map.lookup n env of
+    Just x -> return x
+    Nothing -> throwError $ "Unbound variable " ++ n
+
+eval4 (Plus e1 e2) = do
+  tick
+  i1 <- eval4 e1
+  i2 <- eval4 e2
+  case (i1,i2) of
+    (IntVal a, IntVal b) -> return $ IntVal(a+b)
+    otherwise -> throwError "Type error"
+
+eval4 (Abs n e) = do
+  tick
+  env <- ask
+  return $ FunVal env n e
+
+eval4 (App e1 e2) = do
+  tick
+  val1 <- eval4 e1
+  val2 <- eval4 e2
+  case val1 of
+    FunVal env' n body ->
+      local (const (Map.insert n val2 env')) (eval4 body)
+    otherwise ->
+        throwError "Type error: value is not a function"
+
